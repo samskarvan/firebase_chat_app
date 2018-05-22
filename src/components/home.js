7950bc62b9c89cc458e16b394b6fa72653f542c6
samskarvan <samskarvan@gmail.com>
2018-05-22 12:25:20 -0700

    added nav bar chat button navigates to chatroom logo and home navigate to /

---
+++ src/components/home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default props=>(
+    <div className="center">
+        <h1>Chatty APP</h1>
+        <h4>Bienvenido at Chatty APP. Vamos a charlar!</h4>
+    </div>
+)
---
+++ src/components/nav.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+
+const Nav = props =>{
+    const navStyle={
+        padding: '0 2%'
+    };
+    return(
+        <nav className="blue lighten-3" style={navStyle}>
+            <div className="nav-wrapper">
+            <Link className ="brand-logo" to='/'>Chatty APP</Link>
+            <ul className="right">
+                <li>
+                <Link to='/'>Home</Link>    
+                </li>
+                <li>
+                <Link to="/chat">Chat</Link>    
+                </li>
+            </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Nav
