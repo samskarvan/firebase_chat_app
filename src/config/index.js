b9b782e721f9735c6b58ef5f15a9718c6cd2f6fb
samskarvan <samskarvan@gmail.com>
2018-05-22 14:58:05 -0700

    navBar

---
+++ src/config/index.js
@@ -0,0 +1,10 @@
+export default {
+    firebase:{
+        apiKey: "AIzaSyAOFweA-MBVD0KsoG0dY00mOXvtar4fD9Q",
+        authDomain: "sams-chat-app.firebaseapp.com",
+        databaseURL: "https://sams-chat-app.firebaseio.com",
+        projectId: "sams-chat-app",
+        storageBucket: "sams-chat-app.appspot.com",
+        messagingSenderId: "353462254221"
+      }
+}
